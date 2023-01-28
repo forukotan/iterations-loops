bfa904119f10d24a91c87154d15bcff955a4701a
forukotan <forukotan1@gmail.com>
2023-01-27 22:01:05 -0600

    Add project files.

---
+++ iterations/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace iterations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            */
+            /*for (int i = 0, i <5; i++)
+            {
+                Console.WriteLine($"{i}")
+            }
+            */
+            /*for (int i=0, i<5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            */
+            /*for (int i=0, i<5; i++)
+           {
+               Console.WriteLine($"{i}");
+           }
+           */ /*for (int i=0, i<5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            */ /*for (int i=0, i<5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            */ 
+            /*for (int i=0, i<5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            */ 
+            /*for (int i=0, i<5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            */ 
+            /*for (int i=0, i<5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i=0, i<5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i=0, i<5; i++)
+            {
+                Console.WriteLine($"{i}");
+
+            }
+             for ( int 1=0, i<5; i++)
+            {custom.writeline($"{i}")
+            
+            }
+        }
+    }
+}
